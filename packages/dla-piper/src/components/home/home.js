42a67f69fc180bc9500f1d4542cec62dc341a806
Robert Wilkinson <robert.wilkinson@and.digital>
2022-05-26 11:49:03 +0100

    added ctaInfo section to home page

--- packages/dla-piper/src/components/home/home.js
+++ packages/dla-piper/src/components/home/home.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { connect } from "frontity";
-import { Typography, Stack, Button } from "@mui/material";
+import { connect, styled } from "frontity";
+import { Typography, Stack, Button, List, ListItem, Box, Grid } from "@mui/material";
 import Link from "@frontity/components/link";
+import { Error } from '@mui/icons-material';
 
 const Home = ({ state }) => {
   const data = state.source.get(state.router.link);
@@ -9,7 +10,8 @@ const Home = ({ state }) => {
   const {
     ctaTitle,
     ctaDescription,
-    ctaInfo,
+    ctaInfoTitle,
+    ctaInfoListItems,
     ctaRefugeeLabel,
     ctaVolunteerLabel,
     nonUkTabContent,
@@ -26,6 +28,7 @@ const Home = ({ state }) => {
           fontWeight: 600,
           fontSize: "42px",
           fontFamily: "'Plus Jakarta Sans', sans-serif",
+          margin: '0 0 20px 0',
         }}
       >
         {ctaTitle}
@@ -40,8 +43,37 @@ const Home = ({ state }) => {
       >
         {ctaDescription}
       </Typography>
-      <div dangerouslySetInnerHTML={{ __html: ctaInfo }} />
-      <Stack spacing={2} direction="row">
+      <Box variant='div'
+        sx={{
+          border: '1px solid',
+          borderColor: 'infoColors.border',
+          bgcolor: 'infoColors.background',
+          borderRadius: '8px',
+          padding: '16px',
+          margin: '0 0 20px 0',
+        }}
+      >
+        <Typography 
+          variant='h3'
+          sx={{
+            fontWeight: 600,
+            fontSize: '16px',
+            margin: 0,
+            padding: 0,
+            color: 'textColor.main',
+          }}
+        >
+          {ctaInfoTitle}
+        </Typography>
+        <List sx={{ padding: '0' }}>
+          {ctaInfoListItems.length > 0 &&
+            ctaInfoListItems.split('<br />').map((item, index) => (
+              <ThemedListItem li={item} key={`ctaLI-${index}`} />
+            )
+          )}
+        </List>
+      </Box>
+      <Box variant='div'>
         <Link link="#" style={{ textDecoration: "none" }}>
           <Button
             color="buttonColor"
@@ -50,20 +82,41 @@ const Home = ({ state }) => {
               fontWeight: 400,
               fontSize: "15px",
               fontFamily: "Plus Jakarta Sans",
-              width: "288px",
-              height: "44px",
+              minWidth: "288px",
+              minHeight: "44px",
             }}
           >
             {ctaRefugeeLabel}
           </Button>
         </Link>
         <Link link="#">
-          <Button variant="text">{ctaVolunteerLabel}</Button>
+          <Button 
+            variant="text"
+            sx={{
+              minWidth: "288px",
+              minHeight: "44px",
+            }}
+          >{ctaVolunteerLabel}</Button>
         </Link>
-      </Stack>
+      </Box>
       <div dangerouslySetInnerHTML={{ __html: nonUkTabContent }} />
     </>
   );
 };
+
+const ThemedListItem = ({ li }) => (
+  <ListItem 
+    sx={{ 
+      display: 'flex',
+      flexDirection: 'row',
+      gap: '8px',
+      paddingLeft: '8px',
+      fontSize: '14px' 
+    }}
+  >
+    <Error color='infoIconColor' sx={{ alignSelf: 'flex-start' }} />
+    {li}
+  </ListItem>
+);
 
 export default connect(Home);
