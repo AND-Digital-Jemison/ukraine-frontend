321a50391934b1015776ca2b10f2e8beb5949c33
Hassan Akbar <hassan.akabr@and.digital>
2022-05-26 09:42:17 +0100

    update header styles

--- packages/dla-piper/src/components/home/home.js
+++ packages/dla-piper/src/components/home/home.js
@@ -1,40 +1,66 @@
 import React from "react";
 import { connect } from "frontity";
-import { Typography } from "@mui/material";
+import { Typography, Stack, Button } from "@mui/material";
+import Link from "@frontity/components/link";
 
 const Home = ({ state }) => {
   const data = state.source.get(state.router.link);
   const home = state.source[data.type][data.id];
+  const {
+    ctaTitle,
+    ctaDescription,
+    ctaInfo,
+    ctaRefugeeLabel,
+    ctaVolunteerLabel,
+    nonUkTabContent,
+  } = home.acf;
 
-  const { ctaTitle, ctaDescription, ctaInfo,
-    ctaRefugeeLabel, ctaVolunteerLabel, nonUkTabContent } = home.acf;
   console.log("home.acf", home.acf);
 
   return (
     <>
       <Typography
-        variant='h1'
-        color='textColor.main'
+        variant="h1"
+        color="textColor.main"
         sx={{
           fontWeight: 600,
-          fontSize: '42px',
+          fontSize: "42px",
           fontFamily: "'Plus Jakarta Sans', sans-serif",
-        }}>
+        }}
+      >
         {ctaTitle}
       </Typography>
       <Typography
         paragraph
-        color='textColor.main'
+        color="textColor.main"
         sx={{
           fontWeight: 400,
-          fontSize: '15px',
-        }}>
+          fontSize: "15px",
+        }}
+      >
         {ctaDescription}
       </Typography>
-
       <div dangerouslySetInnerHTML={{ __html: ctaInfo }} />
-      <button>{ctaRefugeeLabel}</button>
-      <a href="#" >{ctaVolunteerLabel}</a>
+      <Stack spacing={2} direction="row">
+        <Link link="#" style={{ textDecoration: "none" }}>
+          <Button
+            color="buttonColor"
+            variant="contained"
+            sx={{
+              fontWeight: 400,
+              fontSize: "15px",
+              fontFamily: "Plus Jakarta Sans",
+              width: "288px",
+              height: "44px",
+            }}
+          >
+            {ctaRefugeeLabel}
+          </Button>
+        </Link>
+        <Link link="#">
+          <Button variant="text">{ctaVolunteerLabel}</Button>
+        </Link>
+      </Stack>
       <div dangerouslySetInnerHTML={{ __html: nonUkTabContent }} />
     </>
   );
