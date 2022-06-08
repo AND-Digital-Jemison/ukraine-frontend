a9542adc2cfc2b2fbe61fa2933a5335f562da8c6
Hassan Akbar <hassan.akabr@and.digital>
2022-06-08 14:54:54 +0100

    add date picker for the date of birth

--- packages/dla-piper/src/components/refugeeFormSteps/whoAreYouStep.js
+++ packages/dla-piper/src/components/refugeeFormSteps/whoAreYouStep.js
@@ -1,27 +1,76 @@
-import { InputField, DropDownList, Step } from "../common/form";
-import { Box } from '@mui/material';
+import { useState, useEffect } from "react";
+import { InputField, DropDownList, DateSelector, Step } from "../common/form";
+import { Box } from "@mui/material";
+import { parse, format, isValid } from "date-fns";
+import { enGB } from "date-fns/locale";
+
+const formatString = "P";
+const isValidDate = (day, month, year) => {
+  return isValid(
+    parse(`${day}/${month}/${year}`, formatString, new Date(), {
+      locale: enGB,
+    })
+  );
+};
 
 const WhoAreYouStep = () => {
+  const [day, setDay] = useState(null);
+  const [month, setMonth] = useState(null);
+  const [year, setYear] = useState(null);
+
+  useEffect(() => {
+    const result = isValidDate(day, month, year);
+    console.log("isValidDate: ", result);
+  });
+
   return (
     <Step label="Who are you?">
-      <Box sx={{
-        display: 'grid',
-        gridTemplateColumns: {
-          mobile: '1fr',
-          tablet: '1fr 1fr'
-        },
-        flex: 1,
-        gap: '20px'
-      }}>
-
-        <InputField label="First name" width='100%' />
-        <InputField label="Last name" width='100%' />
-        <Box sx={{ flex: 1, display: 'flex', flexDirection: 'row', alignItems: 'flex-end', gap: '10px' }}>
-          <DropDownList label="Date of birth" placeholder="dd" width='100%' />
-          <DropDownList  placeholder="mm" width='100%' />
-          <DropDownList  placeholder="yyyy" width='100%' />
+      <Box
+        sx={{
+          display: "grid",
+          gridTemplateColumns: {
+            mobile: "1fr",
+            tablet: "1fr 1fr",
+          },
+          flex: 1,
+          gap: "20px",
+        }}
+      >
+        <InputField label="First Name" width="100%" />
+        <InputField label="Last Name" width="100%" />
+        <Box
+          sx={{
+            flex: 1,
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "flex-end",
+            gap: "10px",
+          }}
+        >
+          <DateSelector
+            setDate={setDay}
+            label="Date of birth"
+            views={["day"]}
+            placeholder="dd"
+            displayFormat="dd"
+            width="100%"
+          />
+          <DateSelector
+            setDate={setMonth}
+            placeholder="mm"
+            views={["month"]}
+            displayFormat="MM"
+            width="100%"
+          />
+          <DateSelector
+            setDate={setYear}
+            placeholder="yyyy"
+            views={["year"]}
+            displayFormat="yyyy"
+            width="100%"
+          />
         </Box>
-        <InputField label="Email" width='100%'/>
+        <InputField label="Email" width="100%" />
       </Box>
     </Step>
   );
