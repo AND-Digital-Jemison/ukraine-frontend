339f99baafba074fde0fb36d19fff218ea0ea8e1
Robert Wilkinson <robert.wilkinson@and.digital>
2022-06-08 17:05:12 +0100

    update WhoAreYouStep to use RHF

--- packages/dla-piper/src/components/refugeeFormSteps/whoAreYouStep.js
+++ packages/dla-piper/src/components/refugeeFormSteps/whoAreYouStep.js
@@ -1,28 +1,75 @@
 import { InputField, DropDownList, Step } from "../common/form";
 import { Box } from '@mui/material';
+import { useForm } from 'react-hook-form';
+import { useSessionStorage } from '../../hooks/useSessionStorage';
+import { useEffect, useMemo } from 'react';
 
 const WhoAreYouStep = () => {
+
+  const [value, setValue] = useSessionStorage('whoAreYou');
+  
+  const { control, reset, handleSubmit } = useForm({
+    defaultValues: useMemo(() => {
+      return value ? value : {
+        firstName: '',
+        lastName: '',
+        dob: '',
+        email: '',
+      };
+    }, [value])
+  });
+  
+  useEffect(() => {
+    console.log(value);
+    reset(value)
+  }, [value])
+
+  const onSubmit = data => {
+    setValue(data);
+  };
+
+
   return (
     <Step label="Who are you?">
-      <Box sx={{
-        display: 'grid',
-        gridTemplateColumns: {
-          mobile: '1fr',
-          tablet: '1fr 1fr'
-        },
-        flex: 1,
-        gap: '20px'
-      }}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Box sx={{
+          display: 'grid',
+          gridTemplateColumns: {
+            mobile: '1fr',
+            tablet: '1fr 1fr'
+          },
+          flex: 1,
+          gap: '20px'
+        }}>
 
-        <InputField label="First name" width='100%' />
-        <InputField label="Last name" width='100%' />
-        <Box sx={{ flex: 1, display: 'flex', flexDirection: 'row', alignItems: 'flex-end', gap: '10px' }}>
-          <DropDownList label="Date of birth" placeholder="dd" width='100%' />
-          <DropDownList  placeholder="mm" width='100%' />
-          <DropDownList  placeholder="yyyy" width='100%' />
+          <InputField
+            name='firstName' 
+            control={control}
+            defaultValue={value ? value?.firstName : 'cheese'}
+            label="First name" 
+            width='100%' 
+          />
+          <InputField 
+            name='lastName'
+            control={control}
+            label="Last name" 
+            width='100%'
+          />
+          <Box sx={{ flex: 1, display: 'flex', flexDirection: 'row', alignItems: 'flex-end', gap: '10px' }}>
+            <DropDownList label="Date of birth" placeholder="dd" width='100%' />
+            <DropDownList  placeholder="mm" width='100%' />
+            <DropDownList  placeholder="yyyy" width='100%' />
+          </Box>
+          <InputField
+            name='email'
+            control={control}
+            label="Email"
+            width='100%'
+          />
         </Box>
-        <InputField label="Email" width='100%'/>
-      </Box>
+
+        <input type="submit" value="Next" />
+      </form>
     </Step>
   );
 };
