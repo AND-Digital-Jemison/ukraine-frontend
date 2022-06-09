5ce5f92b941caa90d2ce0dff426225408dd7fc2f
JohnMercerANDigital <john.mercer@and.digital>
2022-06-09 16:41:41 +0100

    SaveState for FamilyStep

--- packages/dla-piper/src/components/refugeeFormSteps/familyStep.js
+++ packages/dla-piper/src/components/refugeeFormSteps/familyStep.js
@@ -5,26 +5,21 @@ import { Box } from '@mui/material';
 import { useSessionStorage } from '../../hooks/useSessionStorage';
 import { useForm } from 'react-hook-form';
 
-const FamilyStep = () => {
-  const optionsFamily = ["No, I don't have a family member in the UK", "Yes, I have a family member in the UK"];
-  const optionsFamilyType = [
-    "British citizen",
-    "Settled in the UK (also known as indefinite leave to enter or remain, or settled status)",
-    "Refugee or person with humanitarian protection in the UK",
-    "Person with pre-settled status under the EU Settlement Scheme in the UK",
-    "Non of the above"
-  ];
+const schema = {
+  familyMember: '',
+  familyMemberType: '',
+  familyMemberFirstName: '',
+  familyMemberLastName: '',
+  familyMemberRelation: '',
+}
 
-  const [value, setValue] = useSessionStorage('au_family_in_uk');
+const FamilyStep = () => {
+  
+  const [value, setValue] = useSessionStorage('au_family_in_uk', schema);
   
   const { control, reset, handleSubmit } = useForm({
     defaultValues: useMemo(() => {
-      return value ? value : {
-        familyInUk: null,
-        familyMemberFirstName: null,
-        familyMemberLastName: null,
-        familyMemberRelation: null,
-      };
+      return value;
     }, [value])
   });
 
@@ -34,42 +29,56 @@ const FamilyStep = () => {
 
   const onSubmit = data => {
     setValue(data);
+    console.log('data test', data);
   };
 
-  const [familyState, setFamilyState] = useState({
-    option: optionsFamily[0],
-    familyMember: {
-      status: null,
-      firstName: null,
-      lastName: null,
-      relation: null,
-    },
-  });
+  // const [familyState, setFamilyState] = useState({
+  //   option: optionsFamily[0],
+  //   familyMember: {
+  //     status: null,
+  //     firstName: null,
+  //     lastName: null,
+  //     relation: null,
+  //   },
+  // });
 
-  const handleHasFamilyChange = option => {
-    setFamilyState(state => {
-      return { ...state, option: option };
-    })
-  }
+  // const handleHasFamilyChange = option => {
+  //   setFamilyState(state => {
+  //     return { ...state, option: option };
+  //   })
+  // }
 
-  const handleFamilyMemberChange = (key, value) => {
-    setFamilyState(state => {
-      return { ...state, familyMember: { ...state.familyMember, [key]: value } };
-    })
-  }
+  // const handleFamilyMemberChange = (key, value) => {
+  //   setFamilyState(state => {
+  //     return { ...state, familyMember: { ...state.familyMember, [key]: value } };
+  //   })
+  // }
+
+  const optionsFamily = ["No, I don't have a family member in the UK", "Yes, I have a family member in the UK"];
+  const optionsFamilyType = [
+    "British citizen",
+    "Settled in the UK (also known as indefinite leave to enter or remain, or settled status)",
+    "Refugee or person with humanitarian protection in the UK",
+    "Person with pre-settled status under the EU Settlement Scheme in the UK",
+    "Non of the above"
+  ];
 
   return (
     <Step label="Do you have any existing visas for the UK?">
       <form onSubmit={handleSubmit(onSubmit)}>
-        <RadioButtonGroup options={optionsFamily} onChange={handleHasFamilyChange} />
+        <RadioButtonGroup options={optionsFamily} 
+        name='familyMember'
+        control={control} />
         <br />
-        {familyState.option === optionsFamily[1] &&
+        {value.familyMember === optionsFamily[1] &&
           <>
             <Box>
               <RadioButtonGroup
                 label="Which of the following best describes your UK based family member?"
                 options={optionsFamilyType}
-                onChange={value => handleFamilyMemberChange("status", value)}
+                name='familyMemberType'
+                control={control}
+                // onChange={value => handleFamilyMemberChange("status", value)}
               />
               <Box sx={{ padding: '20px 0 0 0' }} >
                 <Label
@@ -91,27 +100,27 @@ const FamilyStep = () => {
               }}
             >
               <InputField 
-                name='firstName'
+                name='familyMemberFirstName'
                 control={control}
                 label='First Name'
                 width={'100%'}
               />
               <InputField 
                 control={control}
-                name='lastName'
+                name='familyMemberLastName'
                 label='Last Name'
                 width={'100%'}
               />
               <DropDownList
                 label='Relation to you'
                 options={relations}
-                onChange={value => handleFamilyMemberChange("relation", value)}
+                // onChange={value => handleFamilyMemberChange("relation", value)}
               />
             </Box>
           </>
         }
 
-        <input type={'submit'}/>
+        <input type="submit" value="Next" /> 
       </form>
     </Step>
   );
