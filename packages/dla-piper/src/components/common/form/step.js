6cef921dd3b6dfae60abc69bbdef30d58af35e32
Robert Wilkinson <robert.wilkinson@and.digital>
2022-06-09 17:27:51 +0100

    removed unneed padding

--- packages/dla-piper/src/components/common/form/step.js
+++ packages/dla-piper/src/components/common/form/step.js
@@ -5,7 +5,7 @@ const Step = ({ label, children }) => {
   return (
     <Box sx={{ display: 'block', flex: 1 }}>
       <Label fontSize={"16px"} fontWeight={600} >{ label }</Label>
-      <Box sx={{ flex: 1, display: 'flex', flexDirection: 'column', padding: '10px 0 20px 0' }}>
+      <Box sx={{ flex: 1, display: 'flex', flexDirection: 'column', padding: '10px 0 0 0' }}>
         { children }
       </Box>
     </Box>
