4a6feb204934f96c977e644df81e4fef068393ac
Robert Wilkinson <robert.wilkinson@and.digital>
2022-06-09 17:40:28 +0100

    add useWatch for conditional logic

--- packages/dla-piper/src/components/refugeeFormSteps/visaStep.js
+++ packages/dla-piper/src/components/refugeeFormSteps/visaStep.js
@@ -1,31 +1,34 @@
 import { Step, RadioButtonGroup } from "../common/form";
-import { useForm } from 'react-hook-form';
+import { useForm, useWatch } from 'react-hook-form';
 import { useSessionStorage } from '../../hooks/useSessionStorage';
 import { useEffect, useMemo } from 'react';
 import { Box } from '@mui/material';
 import { StyledButton } from '../common';
 
+const optionsVisa = ["No, I don't have any visas for the UK", "Yes, I have a visa for the UK"];
+const optionsVisaType = ["Working visa", "Study visa", "Visitor visa", "Indefinite stay visa", "Transit through visa", "Permanent living visa", "Refugee visa", "Stateless person visa"];
+
 const schema = {
-    options: '',
+    hasVisa: '',
     visaType: '',
 }
 
 const VisaStep = ({ onNext, onPrevious }) => {
 
-    const optionsVisa = ["No, I don't have any visas for the UK", "Yes, I have a visa for the UK"];
-    const optionsVisaType = ["Working visa", "Study visa", "Visitor visa", "Indefinite stay visa", "Transit through visa", "Permanent living visa", "Refugee visa", "Stateless person visa"];
-
     const [value, setValue] = useSessionStorage('au_visa_step', schema);
-
     const { control, reset, handleSubmit } = useForm({
         defaultValues: useMemo(() => {
             return value;
         }, [value])
     })
-
     useEffect(() => {
         reset(value)
     }, [value])
+
+    const hasVisa = useWatch({ 
+        control, 
+        name: 'hasVisa' 
+    });
 
     const onSubmit = data => {
         setValue(data);
@@ -47,10 +50,10 @@ const VisaStep = ({ onNext, onPrevious }) => {
         <Step label="Do you have any existing visas for the UK?">
             <form onSubmit={handleSubmit(onSubmit)}>
                 <RadioButtonGroup options={optionsVisa}
-                    name='options'
+                    name='hasVisa'
                     control={control} />
                 <br />
-                {value.options === optionsVisa[1] &&
+                {hasVisa === optionsVisa[1] &&
                     <RadioButtonGroup label="Which visa do you have?"
                         options={optionsVisaType}
                         name='visaType'
