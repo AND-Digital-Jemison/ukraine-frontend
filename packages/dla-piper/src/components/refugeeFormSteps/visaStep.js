d6dc09b28c90bcbd9a3a9dc146e47f7908578fc3
JohnMercerANDigital <john.mercer@and.digital>
2022-06-09 15:38:28 +0100

    updated VisaStep for saveState

--- packages/dla-piper/src/components/refugeeFormSteps/visaStep.js
+++ packages/dla-piper/src/components/refugeeFormSteps/visaStep.js
@@ -1,34 +1,50 @@
 import { Step, RadioButtonGroup } from "../common/form";
-import { useState } from "react";
+import { useForm } from 'react-hook-form';
+import { useSessionStorage } from '../../hooks/useSessionStorage';
+import { useEffect, useMemo } from 'react';
+
+const schema = {
+    options: '',
+    visaType: '',
+}
 
 const VisaStep = () => {
+    
+    const [value, setValue] = useSessionStorage('au_visa_step', schema);
+
+    const { control, reset, handleSubmit } = useForm({
+        defaultValues: useMemo(() => {
+            return value;
+        }, [value])
+    })
+
+    useEffect(() => {
+        reset(value)
+    }, [value])
+
+    const onSubmit = data => {
+        setValue(data);
+        console.log('data test', data);
+    }
+
     const optionsVisa = ["No, I don't have any visas for the UK", "Yes, I have a visa for the UK"];
     const optionsVisaType = ["Working visa", "Study visa", "Visitor visa", "Indefinite stay visa", "Transit through visa", "Permanent living visa", "Refugee visa", "Stateless person visa"];
 
-    const [visaState, setVisaState] = useState({
-        option: optionsVisa[0],
-        visaType: null,
-    });
-
-    const handleHasVisaChange = option => {
-        setVisaState(state => {
-            return{...state, option: option}
-        })
-    }
-
-    const handleVisaTypeChange = option => {
-        setVisaState(state => {
-            return{...state, visaType: option}
-        })
-    }
-
     return (
         <Step label="Do you have any existing visas for the UK?">
-            <RadioButtonGroup options={optionsVisa} onChange={handleHasVisaChange}/>
-            <br/>
-            {visaState.option === optionsVisa[1] &&
-                <RadioButtonGroup label="Which visa do you have?" options={optionsVisaType} onChange={handleVisaTypeChange}/>
-            }
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <RadioButtonGroup options={optionsVisa} 
+                    name='options'
+                    control={control}/>
+                <br/>
+                {value.options === optionsVisa[1] &&
+                    <RadioButtonGroup label="Which visa do you have?" 
+                        options={optionsVisaType}
+                        name='visaType'
+                        control={control}/>
+                }
+                <input type="submit" value="Next" />
+            </form>
         </Step>
     );
 }
