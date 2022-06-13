d8205d921dfb0646815b1adf584c769c434a0f71
JohnMercerANDigital <john.mercer@and.digital>
2022-06-13 11:31:45 +0100

    Confirmation Page

--- packages/dla-piper/src/components/confirmation/confirmation.js
+++ packages/dla-piper/src/components/confirmation/confirmation.js
@@ -1,17 +1,60 @@
 import { connect } from 'frontity';
-import { MaxRestraintWrapper } from '../common';
-import { Box, Typography } from '@mui/material';
+import {
+  MaxRestraintWrapper,
+  PageHeader, 
+  ContentBlockWrapper,
+} from '../common';
+import { Box, Typography, styled } from '@mui/material';
 import { DoneSharp } from '@mui/icons-material';
 
-const Confirmation = ({ state }) => {
+const Confirmation = ({ state, libraries }) => {
   const data = state.source.get(state.router.link);
+  const confirmation = state.source[data.type][data.id];
+  const {
+    confirmTitle,
+    confirmNextTitle,
+    confirmNextInfo,
+    confirmDataTitle,
+    confirmDataInfo,
+  } = confirmation.acf;
 
+  const Html2React = libraries.html2react.Component;
 
   return (
     <>
+      <PageHeader>
+        <MaxRestraintWrapper>
+          <Typography
+            variant="h1"
+            color="textColor.main"
+            sx={{
+              fontWeight: 600,
+              fontSize: "42px",
+              fontFamily: "'Plus Jakarta Sans', sans-serif",
+              margin: "0 0 20px 0",
+            }}
+          >
+            {confirmTitle}
+          </Typography>
+          <ReceivedNotification />
+        </MaxRestraintWrapper>
+      </PageHeader>
       <MaxRestraintWrapper>
-        <ReceivedNotification />
-
+        <Box sx={{
+          padding: '20px',
+          bgcolor: '#F8F8F8'
+        }} >
+          <Typography>{confirmNextTitle}</Typography>
+          <ContentBlockWrapper>
+            <Html2React html={confirmNextInfo}/>
+          </ContentBlockWrapper>
+        </Box>
+      </MaxRestraintWrapper>
+      <MaxRestraintWrapper>
+        <Typography>{confirmDataTitle}</Typography>
+        <ContentBlockWrapper>
+          <Html2React html={confirmDataInfo}/>
+        </ContentBlockWrapper>
       </MaxRestraintWrapper>
     </>
   )
