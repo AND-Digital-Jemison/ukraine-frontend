fba320dde1be62537e67d6797b4daa36b1c89b91
Hassan Akbar <hassan.akabr@and.digital>
2022-07-08 14:46:47 +0100

    Show a validation message for future dates for DOB

--- packages/dla-piper/src/components/refugeeFormSteps/whoAreYouStep.js
+++ packages/dla-piper/src/components/refugeeFormSteps/whoAreYouStep.js
@@ -1,32 +1,46 @@
 import * as yup from 'yup';
 import { useEffect, useMemo } from 'react';
 import { connect } from 'frontity';
-import { format, toDate } from "date-fns";
+import { format, toDate } from 'date-fns';
 import { useForm } from 'react-hook-form';
-import { DatePicker, InputField, Step } from "../common/form";
+import { DatePicker, InputField, Step } from '../common/form';
 import { Box } from '@mui/material';
-import Link from "@frontity/components/link";
+import Link from '@frontity/components/link';
 import { StyledButton } from '../common';
 import getFormButtonLabels from './getFormButtonLabels';
 import { useSessionStorage } from '../../hooks/useSessionStorage';
 import { useYupResolver } from '../../hooks';
 
 // we are using this naming convention to follow the shape of the data
-// that legal connection is expecting, yes we would like it to be 
-// camelCase, but we can't always have what we want :( 
+// that legal connection is expecting, yes we would like it to be
+// camelCase, but we can't always have what we want :(
 const schema = {
   firstname: '',
   lastname: '',
   date_of_birth: null,
   email: '',
-}
+};
+
+const today = new Date();
+today.setHours(23, 59, 59, 0);
 
 const validationSchema = yup.object().shape({
-  firstname: yup.string().max(64, 'cannot excide 64 characters').required('First name is required'),
-  lastname: yup.string().max(64, 'cannot excide 64 characters').required('Last name is required'),
-  date_of_birth: yup.date().typeError('Date of birth is invalid').required('Date of birth is required').nullable(),
+  firstname: yup
+    .string()
+    .max(64, 'cannot excide 64 characters')
+    .required('First name is required'),
+  lastname: yup
+    .string()
+    .max(64, 'cannot excide 64 characters')
+    .required('Last name is required'),
+  date_of_birth: yup
+    .date()
+    .typeError('Date of birth is invalid')
+    .max(today, 'Date of birth cannot be a future date')
+    .required('Date of birth is required')
+    .nullable(),
   email: yup.string().email('Email is invalid').required('Email is required'),
-})
+});
 
 const WhoAreYouStep = ({ state, onNext }) => {
   const { theme } = state;
@@ -35,22 +49,27 @@ const WhoAreYouStep = ({ state, onNext }) => {
   const [value, setValue] = useSessionStorage('au_who_are_you', schema);
 
   const resolver = useYupResolver(validationSchema);
-  const { control, reset, handleSubmit, formState: { errors } } = useForm({
+  const {
+    control,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
     resolver,
     defaultValues: useMemo(() => {
       return value;
-    }, [value])
+    }, [value]),
   });
 
   useEffect(() => {
-    reset(value)
-  }, [value])
+    reset(value);
+  }, [value]);
 
-  const onSubmit = data => {
+  const onSubmit = (data) => {
     const payload = {
       ...data,
-      date_of_birth: format(toDate(data.date_of_birth), 'yyyy-MM-dd')
-    }
+      date_of_birth: format(toDate(data.date_of_birth), 'yyyy-MM-dd'),
+    };
     setValue(payload);
 
     if (!onNext) {
@@ -60,27 +79,29 @@ const WhoAreYouStep = ({ state, onNext }) => {
   };
 
   return (
-    <Step label="Who are you?">
+    <Step label='Who are you?'>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <Box sx={{
-          display: 'grid',
-          gridTemplateColumns: {
-            mobile: '1fr',
-            tablet: '1fr 1fr'
-          },
-          flex: 1,
-          gap: '20px'
-        }}>
+        <Box
+          sx={{
+            display: 'grid',
+            gridTemplateColumns: {
+              mobile: '1fr',
+              tablet: '1fr 1fr',
+            },
+            flex: 1,
+            gap: '20px',
+          }}
+        >
           <InputField
             name='firstname'
             control={control}
-            label="First name"
+            label='First name'
             width='100%'
           />
           <InputField
             name='lastname'
             control={control}
-            label="Last name"
+            label='Last name'
             width='100%'
           />
           <DatePicker
@@ -92,24 +113,25 @@ const WhoAreYouStep = ({ state, onNext }) => {
           <InputField
             name='email'
             control={control}
-            label="Email"
+            label='Email'
             width='100%'
           />
         </Box>
 
-        <Box sx={{ display: 'flex', justifyContent: 'space-between', padding: '20px 0 0 0' }}>
-          <Link link={`/home/${currentLanguage}/`} style={{ textDecoration: "none" }}>
-            <StyledButton
-              label={back}
-              width={'125px'}
-              variant="outlined"
-            />
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            padding: '20px 0 0 0',
+          }}
+        >
+          <Link
+            link={`/home/${currentLanguage}/`}
+            style={{ textDecoration: 'none' }}
+          >
+            <StyledButton label={back} width={'125px'} variant='outlined' />
           </Link>
-          <StyledButton
-            label={next}
-            width={'125px'}
-            submit
-          />
+          <StyledButton label={next} width={'125px'} submit />
         </Box>
       </form>
     </Step>
