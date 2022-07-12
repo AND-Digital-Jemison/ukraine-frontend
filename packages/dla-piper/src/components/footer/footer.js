d74d196693f1837fd1040577f82cfbc85fa225ad
Robert Wilkinson <robert.wilkinson@and.digital>
2022-07-12 15:10:15 +0100

    update with figma design

--- packages/dla-piper/src/components/footer/footer.js
+++ packages/dla-piper/src/components/footer/footer.js
@@ -1,24 +1,75 @@
-import { styled, Box } from '@mui/material';
+import { styled, Box, Typography } from '@mui/material';
+import Link from '@frontity/components/link';
 import { MaxRestraintWrapper } from '../common';
 
 const Footer = () => {
 
   return (
     <FooterWrapper>
-      <MaxRestraintWrapper>
         <Box sx={{
           display: 'grid',
           gridTemplateColumns: {
-            desktop: '1fr 1fr 1fr',
-            tablet: '1fr 1fr',
+            desktop: '25fr 45fr 30fr',
             mobile: '1fr',
           },
+          gap: '2rem',
         }}>
-          <p>col</p>
-          <p>col</p>
-          <p>col</p>
+          <Box 
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+              textAlign: 'center',
+            }}
+          >
+            <Typography variant='h6'>
+              Ukraine Advice Project
+            </Typography>
+            <Typography variant='p'>
+              Powered by
+            </Typography>
+          </Box>
+
+          <Box 
+            sx={{
+              display: 'flex',
+              flexDirection: {
+                desktop: 'row',
+                mobile: 'column',
+              },
+              gap: '1rem',
+              alignItems: 'center',
+              justifyContent: {
+                desktop: 'flex-start',
+                mobile: 'center',
+              }
+            }}
+          >
+            <FooterLink link='https://www.dlapiper.com/' text='DLA Piper' type='external' />
+            <FooterLink link='https://www.legalconnection.co/' text='Legal Connection' type='external' />
+          </Box>
+          
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: {
+                desktop: 'row',
+                mobile: 'column',
+              },
+              gap: '1rem',
+              alignItems: 'center',
+              justifyContent: {
+                desktop: 'flex-start',
+                mobile: 'center',
+              }
+            }}
+          >
+            <FooterLink link='#' text='FAQs' type='internal' />
+            <FooterLink link='#' text='Cookie Policy' type='internal' />
+            <FooterLink link='#' text='Legal Statement' type='internal' />
+          </Box>
         </Box>
-      </MaxRestraintWrapper>
     </FooterWrapper>
   )
 };
@@ -28,5 +79,24 @@ const FooterWrapper = styled('footer')({
   padding: '48px 0 96px 0',
 
 });
+
+const FooterLink = ({ link, text, type }) => {
+  return (
+    <Link link={link}
+      style={{
+        textDecoration: type === 'external' ? 'underline': 'none',
+        color: '#2C6ECB',
+      }}
+    >
+      <Typography variant='p'
+        sx={{
+          color: type === 'external' ? '#2C6ECB': '#202223',
+        }}
+      >
+        { text }
+      </Typography>
+    </Link>
+  )
+}
 
 export default Footer;
